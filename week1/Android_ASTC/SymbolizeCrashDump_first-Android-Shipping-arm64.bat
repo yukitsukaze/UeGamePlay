cbe641ccf4996814ac1006e9dcf0c217177afd51
yukitsukaze <138577402+yukitsukaze@users.noreply.github.com>
2024-11-19 17:56:04 +0800

    update：week1

---
+++ week1/Android_ASTC/SymbolizeCrashDump_first-Android-Shipping-arm64.bat
@@ -0,0 +1,16 @@
+@echo off
+IF %1.==. GOTO NoArgs
+setlocal
+set NDK_ROOT=%ANDROID_NDK_ROOT%
+if "%ANDROID_NDK_ROOT%"=="" set NDK_ROOT=""
+set NDKSTACK=%NDK_ROOT%\ndk-stack.cmd
+
+%NDKSTACK% -sym first_Symbols_v1/firstarm64 -dump "%1" > first_SymbolizedCallStackOutput.txt
+
+goto:eof
+
+
+:NoArgs
+echo.
+echo Required argument missing, pass a dump of adb crash log. (SymboliseCallStackDump C:\adbcrashlog.txt)
+pause
